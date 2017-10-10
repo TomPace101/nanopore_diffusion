732647a5c0de4b3ec500c5237d95f416462f64f6
Tom Pace <tom.pace@uky.edu>
2017-10-10 09:28:03 -0400

    First draft of the ParameterSet class

--- src/useful.py
+++ src/useful.py
@@ -45,3 +45,65 @@ def writepickle(obj,fpath):
   with open(fpath, 'wb') as fp:
     pickle.dump(obj,fp,pickle_protocol)
   return
+
+#-------------------------------------------------------------------------------
+#ParameterSet
+
+class ParameterSet:
+  """A base class for defining sets of related parameters.
+  Each subclass should use __slots__ to define its parameters."""
+  def __init__(self,**kwd):
+    self.__dict__.udpate(kwd)
+  @classmethod
+  def from_yaml(cls,fpath):
+    """Read ParameterSet from a yaml file.
+    Arguments:
+      fpath = path to the yaml file to read in
+        This must be a single-document yaml file,
+        and it is assumed to be structured as a single dictionary at the top level.
+    Returns:
+      pset = a ParameterSet object as defined by the contents of the yaml file"""
+    d=readyaml(fpath)
+    return cls(**d)
+  def to_yaml(self,fpath):
+    """Write ParameterSet to a yaml file.
+    Arguments:
+      fpath = path to the yaml file to write
+        This will be a single-document yaml file, containing a dictionary (potentially of other dictionaries).
+        The file will be overwritten if it already exists.
+    No return value."""
+    writeyaml(self.__dict,fpath)
+    return
+  @classmethod
+  def all_from_yaml(cls,fpath):
+    """Generator to read a list of ParameterSet objects from a yaml file.
+    Arguments:
+      fpath = path to the yaml file to read in
+        This must be a multi-document yaml file,
+        and each document is assumed to be structured as a single dictionary at the top level.
+    Each call yields:
+      pset = a ParameterSet object as defined by one document in the yaml file"""
+    with open(fpath,'r') as fp:
+      dat=fp.read()
+      gen=yaml.load_all(dat)
+    for obj in gen:
+      yield obj
+  @classmethod
+  def from_pickle(cls,fpath):
+    """Read ParameterSet from a pickle file.
+    Arguments:
+      fpath = path to the pickle file to read in
+        This file should be a mapping type at the top level.
+    Returns:
+      pset = a ParameterSet object as defined by the contents of the pickle file."""
+    d=readpickle(fpath)
+    return cls(**d)
+  def to_pickle(self,fpath):
+    """Write ParameterSet to a pickle file.
+    Arguments:
+      fpath = path to the pickle file to write
+        The file will be overwritten if it already exists.
+    No return value."""
+    writepickle(self.__dict__,fpath)
+    return
+  ##TODO: read and write from ini file
