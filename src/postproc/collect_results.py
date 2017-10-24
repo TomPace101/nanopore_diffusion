7de29252f994dd733f5725cc111abe49ef52cb7e
Tom Pace <tom.pace@uky.edu>
2017-10-24 14:42:39 -0400

    Removing unused code in collect_results

--- src/postproc/collect_results.py
+++ src/postproc/collect_results.py
@@ -94,34 +94,3 @@ def do_collection(inputfiles,outfpath):
   df = dicts_to_dataframe(alldicts)
   df.to_pickle(outfpath)
   return
-
-#-------------------------------------------------------------------------------
-def read_all_dicts(rundict):
-  #Dictionary of all meshes, by name
-  allmeshes={}
-  for doc in useful.readyaml_multidoc(rundict['meshparams']):
-    allmeshes[doc['meshname']]=doc
-  #Dictionary of all models, by name
-  allmodels={}
-  for doc in useful.readyaml_multidoc(rundict['modelparams']):
-    allmodels[doc['modelname']]=doc
-  return allmeshes, allmodels
-
-def list_result_files(rundict):
-  allmeshes, allmodels=read_all_dicts(rundict)
-  infiles=[osp.join(solnfolder,mname,'results.yaml') for mname in allmodels.keys()]
-  return infiles
-
-def gen_dataframe(rundict):
-  infiles=list_result_files(rundict)
-  alldicts=[]
-  for infpath in infiles:
-    alldicts.append(useful.readyaml(infpath))
-  df = pd.DataFrame(alldicts)
-  return df
-
-
-def collectall(rundict):
-  outfpath=osp.join(solnfolder,rundict['master']+'.pkl.gz')
-  df = gen_dataframe(rundict)
-  df.to_pickle(outfpath)
