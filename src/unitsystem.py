ae5d46ae955a525b2bc6968204a6ea465fd6e7af
Tom Pace <tom.pace@uky.edu>
2018-01-25 16:16:07 -0500

    Initially complete unitsystem module

--- src/unitsystem.py
+++ src/unitsystem.py
@@ -15,14 +15,41 @@ avogadro=pc['Avogadro constant'][0]
 #This way, inputs are specfied as X*unit to convert them from "unit" to the corresponding model uit
 #and outputs are converted to other units as Y/unit.
 
-#Concentration
-molar=avogadro/1e24
-millimolar=molar*1e-3
-micromolar=molar*1e-6
+#Length
+meter = 1e9
+micron = 1e3
+nanometer = 1
+
+#Number of particles
+particle = 1
+mole = avogadro
+dozen = 12
 
 #Time
-second=1e-9
-millisecond=1e-6
-microsecond=1e-3
 nanosecond=1
+microsecond=1e3
+millisecond=1e6
+second=1e9
 
+#Energy
+electronVolt = 1
+joule = 1.0/pc['electron volt'][0]
+
+#Temperature
+kelvin = 1
+
+#Electric charge
+coulomb = 1.0/pc['elementary charge'][0]
+
+#Concentration
+molar = avogadro/1e24
+millimolar = molar*1e-3
+micromolar = molar*1e-6
+
+#Electric potential
+volt = 1
+millivolt = 1e-3
+
+#Vacuum permittivity
+eps_0_SI = pc['electric constant'][0]
+eps_0 = eps_0_SI * coulomb / volt / meter
