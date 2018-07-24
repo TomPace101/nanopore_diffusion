4216b34fc6f4761de4752ecdc86b78375ee1bed6
Tom Pace <tom.pace@uky.edu>
2018-07-24 09:25:34 -0400

    checkpoint progress on run.py

---
+++ src/run.py
@@ -0,0 +1,29 @@
+"""Command-line support for running requests"""
+
+#Standard library
+import argparse
+
+#Site packages
+import yaml
+##from ruamel.yaml import YAML
+##yaml=YAML()
+
+#Local
+import folderstructure as FS
+import fpath
+
+#Handle command-line execution
+if __name__ == '__main__':
+  #Parse command line arguments
+  parser = argparse.ArgumentParser(description=globals()['__doc__'])
+  parser.add_argument('requestfile', help="Path to file containing the request(s) to run")
+  #TODO: allow selecting a subset of the requests?
+  cmdline=parser.parse_args()
+  requestfile=fpath.fpath(cmdline.requestfile,isFile=True)
+  
+  #Confirm that specified request file exists
+  assert requestfile.exists(), "Could not find specified request file %s"%requestfile.fullpath
+  
+  #Load all of the requests
+  ##TODO
+  
