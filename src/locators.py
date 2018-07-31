b2baddfca79dc956b8a04fc382edf22b243ebf4f
Tom Pace <tom.pace@uky.edu>
2018-07-31 15:24:34 -0400

    Data locators are Path subclasses again, and there's only one for now.

--- src/locators.py
+++ src/locators.py
@@ -9,30 +9,18 @@ from __future__ import print_function, division #Python 2 compatibility
 import filepath
 import folderstructure as FS
 
-class LocatorBase(object):
-  """Dummy class, just to make type checking easier"""
-  pass
-
-class InDataFolder(LocatorBase):
-  """File location relative to ``datafolder``
+class DataFile(filepath.Path):
+  """Data file location relative to ``datafolder``
   
   The location of ``datafolder`` is specified by the environment variable ``DATALOC``.
   If this environment variable does not exist, a default value is provided.
   See ``folderstructure.py``."""
   parentpath=FS.datafolder
-  def __init__(self,*args,**kwargs):
-    self.subpath=filepath.Path(*args,**kwargs)
-  def path(self,req):
-    return self.parentpath / self.subpath
+  def __new__(cls, *args, **kwargs):
+    subpath=filepath.Path(*args,**kwargs)
+    self = cls.parentpath / subpath
+    return self
 
-class InRequestFolder(LocatorBase):
-  """File in location determined by the Request that needs it"""
-  def __init__(self,*args,**kwargs):
-    ##TODO
-    pass
-  def path(self,req):
-    ##TODO
-    pass
 
-yaml_classes=[InDataFolder]
+yaml_classes=[DataFile]
 
