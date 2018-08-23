1ed59f06182bb6220d555bcb70124336996e0e7f
Tom Pace <tom.pace@uky.edu>
2018-08-23 17:38:02 -0400

    First attempt at base class for all shell commands

---
+++ src/simproc/requesthandler/shell.py
@@ -0,0 +1,27 @@
+"""Base class for requests that execute shell commands."""
+
+#Standard library
+from __future__ import print_function, division #Python 2 compatibility
+from subprocess import call
+
+#This package
+from . import request
+from . import yaml_manager
+
+class ShellCommandRequest(request.Request):
+  """Base class for requests that execute shell commands
+  
+  Subclasses must do the following:
+  
+  - define _outputfile_attrs and/or _more_outputfiles, either as class or instance attributes
+  - define a property attribute cmd_str that provides the shell command to be executed, as a string"""
+
+  def run(self):
+    #Create directories for output files if necessary
+    allpaths=[getattr(self,oattr) for oattr in getattr(self,_outputfile_attrs,[])]
+    allpaths+=getattr(self,_more_outputfiles,[])
+    for fpath in allpaths:
+      fpath.assure_dir()
+    #Run the shell command
+    call(self.cmd_str,shell=True)
+  
