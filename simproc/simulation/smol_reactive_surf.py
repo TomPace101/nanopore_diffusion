c64ebbd815f2e574029bacadb85d3023b5e3fb5c
Tom Pace <tom.pace@uky.edu>
2019-06-06 17:48:41 -0400

    minor tweaks

--- simproc/simulation/smol_reactive_surf.py
+++ simproc/simulation/smol_reactive_surf.py
@@ -178,14 +178,39 @@ class SUSimulator(simrequest.SimulationRequest):
       cbar_single=fem.project(cbar,self.V_scalar,solver_type="cg",preconditioner_type="amg")
       self.cbarlist.append(cbar_single)
 
-  def calcflux(self, solnattr='soln', idx=None, fluxattr='flux'):
+  def calcflux(self, solnattr='soln', idx=None, attrpath='flux'):
     """Flux as vector field (new attribute)"""
     soln=getattr(self,solnattr)
     if idx is not None:
       soln=soln[idx]
     expr=-self.Dbar_proj[idx]*fem.grad(soln)
     fluxres=fem.project(expr,self.V_vec,solver_type="cg",preconditioner_type="amg") #Solver and preconditioner selected to avoid UMFPACK "out of memory" error (even when there's plenty of memory)
-    setattr(self,fluxattr,fluxres)
+    self.set_nested(attrpath,fluxres)
+    return
+
+  def fluxintegral(self,pfacet,attrpath,internal=False,fluxsign=None,normalvar=None,fluxattr='flux',idx=None):
+    """Flux integral over specified facet
+
+    Arguments:
+      pfacet = physical facet number for flux measurement
+      attrpath = attribute path for storage of the integral
+      internal = boolean, default False, True to use internal boundary, False for external
+      fluxsign = '+' or '-' to specify which direction normal to the facet for flux calculation
+        Required only if internal==True
+      fluxattr = name of attribute containing the flux field for integration
+    No return value.
+    No output files."""
+    n=fem.FacetNormal(self.meshinfo.mesh)
+    if internal:
+      integral_type='interior_facet'
+      assert fluxsign=='+' or fluxsign=='-', "Invalid fluxsign: %s"%str(fluxsign)
+      this_n=n(fluxsign)
+    else:
+      integral_type='exterior_facet'
+      this_n=n
+    this_ds=fem.Measure(integral_type,domain=self.meshinfo.mesh,subdomain_data=self.meshinfo.facets)
+    totflux=fem.assemble(fem.dot(getattr(self,fluxattr),this_n)*this_ds(pfacet))
+    self.set_nested(attrpath,totflux)
     return
 
 
