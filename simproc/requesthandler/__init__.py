62a3bc22ad4befc92c3e6b5ab2f70d97adf53e2f
Tom Pace <tom.pace@uky.edu>
2019-06-25 14:20:21 -0400

    sort and add to submodule imports of requesthandler

--- simproc/requesthandler/__init__.py
+++ simproc/requesthandler/__init__.py
@@ -1,16 +1,19 @@
 #Import all modules that define classes loadable from yaml
-from . import filepath
-from . import locators
-from . import requestfile
-from . import simultaneous
-from . import customization
-from . import shell
-from . import mpi_run
-from . import templates
-from . import generate
 from . import cleanup
 from . import comparison
+from . import customization
 from . import debug
+from . import filepath
+from . import generate
+from . import locators
+from . import mpi_run
+from . import nested
+from . import request
+from . import requestfile
+from . import shell
+from . import simultaneous
+from . import templates
+from . import yaml_manager
 
 #Modules for doctests
 doctest_modules=[filepath, debug, comparison]
