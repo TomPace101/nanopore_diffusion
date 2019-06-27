a65b9ad96f61d2d7b60f1e929318ac1bbc0fef0e
Tom Pace <tom.pace@uky.edu>
2019-06-27 09:52:48 -0400

    Flesh out plotseries a little bit.

--- simproc/postproc/plotseries.py
+++ simproc/postproc/plotseries.py
@@ -1,3 +1,5 @@
+"""Defining the data series that go on plots"""
+
 
 class PlotSeries:
   """Data for a single series on a plot
@@ -9,10 +11,11 @@ class PlotSeries:
     - label = legend label
     - metadata = other parameters needed to identify the data series
   """
-  _expected_attrs=['xvals','yvals','label','metadata']
-  def __init__(self,**kwargs):
-    for k,v in kwargs.items():
-      setattr(self,k,v)
+  def __init__(self,xvals,yvals,label=None,metadata=None):
+    self.xvals=xvals
+    self.yvals=yvals
+    self.label=label
+    self.metadata=metadata
   def add_to_axes(self,ax,fmt,newlabel=None,**kwd):
     """Plot this series on the specified axes
 
@@ -29,7 +32,26 @@ class PlotSeries:
 
       - The result of call to ax.plot"""
     if newlabel is None:
-      label=self.label
+      label=getattr(self,'label',None)
+      if label is None:
+        label=''
     else:
       label=newlabel
     return ax.plot(self.xvals,self.yvals,fmt,label=label,**kwd)
+
+##Should this be a classmethod of PlotSeries instead of a class?
+#argue no: not loadable from yaml then
+#argument yes: don't need to load from yaml
+class PlotDataFrameSeries:
+  """Define a PlotSeries from a DataFrame"""
+  def __new__(cls,df,xcol,ycol,query=None,label=None,metadata=None):
+    #Select particular rows if requested
+    if query is None or len(query)==0:
+      qdf=df
+    else:
+      qdf=df.query(query)
+    #Get the data for the series from the dataframe
+    xvals=qdf[xcol]
+    yvals=qdf[ycol]
+    #Instantiate!
+    return PlotSeries(xvals=vals,yvals=yvals,label=label,metadata=metadata)
