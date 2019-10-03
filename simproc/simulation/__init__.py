78bc0beace0462d3f9e8b381195715bd96f44f5a
Tom Pace <TomPace101@gmail.com>
2019-10-03 17:34:47 -0400

    File that combines 2D and 3D homogenization

--- simproc/simulation/__init__.py
+++ simproc/simulation/__init__.py
@@ -4,5 +4,6 @@ from . import unitsystem
 from . import simrequest
 from . import fickian_homog2D
 from . import fickian_homog3D
+from . import fickian_homog
 from . import lin_pb
 from . import smol_reactive_surf
