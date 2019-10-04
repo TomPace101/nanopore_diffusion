23e042ebfe7c636d1f9de6cc43f073b9be49bf14
Tom Pace <TomPace101@gmail.com>
2019-10-03 21:05:48 -0400

    Fix typo

--- simproc/simulation/__init__.py
+++ simproc/simulation/__init__.py
@@ -5,6 +5,6 @@ from . import simrequest
 from . import fickian_homog2D
 from . import fickian_homog3D
 from . import fickian_homog
-from . import smol_homog.py
+from . import smol_homog
 from . import lin_pb
 from . import smol_reactive_surf
