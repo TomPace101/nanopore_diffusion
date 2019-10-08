b80e553f5a317e55813aed030491b5d188c96f3e
Tom Pace <tom.pace@uky.edu>
2019-10-08 11:32:26 -0400

    Remove the old 2D- and 3D-specific homogenization modules

--- simproc/simulation/__init__.py
+++ simproc/simulation/__init__.py
@@ -2,8 +2,6 @@
 # Package modules
 from . import unitsystem
 from . import simrequest
-from . import fickian_homog2D
-from . import fickian_homog3D
 from . import fickian_homog
 from . import smol_homog
 from . import lin_pb
