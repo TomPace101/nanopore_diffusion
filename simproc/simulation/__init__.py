b0eb1307f8cf364bd0cb663d1dbfa3f1da82a981
Tom Pace <tom.pace@uky.edu>
2019-10-08 11:48:19 -0400

    Move UserExpression subclasses to their own module

--- simproc/simulation/__init__.py
+++ simproc/simulation/__init__.py
@@ -2,6 +2,8 @@
 # Package modules
 from . import unitsystem
 from . import simrequest
+from . import expressions
+from . import periodic_boundaries
 from . import fickian_homog
 from . import smol_homog
 from . import lin_pb
--- simproc/simulation/cellmapping.py
+++ simproc/simulation/cellmapping.py
@@ -9,44 +9,7 @@ import fenics as fem
 #This package
 from ..requesthandler import yaml_manager
 from . import simrequest
-
-class VaryingScalarByCell(fem.UserExpression):
-  def __init__(self,meshfunc,prop_map,**kwargs):
-    self.meshfunc=meshfunc
-    self.prop_map=prop_map
-    super().__init__(**kwargs)
-  def value_shape(self):
-    return ()
-  def eval_cell(self, values, x, cell):
-    key=self.meshfunc[cell.index]
-    values[0]=self.prop_map[key]
-
-class VaryingVectorByCell(fem.UserExpression):
-  def __init__(self,meshfunc,prop_map,spatial_dims,**kwargs):
-    self.meshfunc=meshfunc
-    self.prop_map=prop_map
-    self.spatial_dims=spatial_dims
-    super().__init__(**kwargs)
-  def value_shape(self):
-    return (self.spatial_dims,)
-  def eval_cell(self, values, x, cell):
-    key=self.meshfunc[cell.index]
-    for i in range(self.spatial_dims):
-        values[i]=self.prop_map[key][i]
-
-class VaryingMatrixByCell(fem.UserExpression):
-  def __init__(self,meshfunc,prop_map,spatial_dims,**kwargs):
-    self.meshfunc=meshfunc
-    self.prop_map=prop_map
-    self.spatial_dims=spatial_dims
-    super().__init__(**kwargs)
-  def value_shape(self):
-    return (self.spatial_dims,self.spatial_dims)
-  def eval_cell(self, values, x, cell):
-    key=self.meshfunc[cell.index]
-    for i in range(self.spatial_dims):
-      for j in range(self.spatial_dims):
-        values[self.spatial_dims*j+i]=self.prop_map[key][i][j]
+from . import expressions
 
 
 _MappingConditions_props_schema_yaml="""#MappingConditions
@@ -96,13 +59,13 @@ class CellMappingSimulator(simrequest.SimulationRequest):
     ftype=getattr(conditions,'functiontype','scalar').lower()
     if ftype == 'scalar':
       self.V = fem.FunctionSpace(self.meshinfo.mesh,'P', conditions.elementorder)
-      self.expr = VaryingScalarByCell(self.meshinfo.cells,conditions.mapping,degree=0)
+      self.expr = expressions.VaryingScalarByCell(self.meshinfo.cells,conditions.mapping,degree=0)
     elif ftype == 'vector':
       self.V = fem.VectorFunctionSpace(self.meshinfo.mesh, 'P', conditions.elementorder)
-      self.expr = VaryingVectorByCell(self.meshinfo.cells,conditions.mapping,spatial_dims,degree=0)
+      self.expr = expressions.VaryingVectorByCell(self.meshinfo.cells,conditions.mapping,spatial_dims,degree=0)
     elif ftype == 'matrix':
       self.V = fem.TensorFunctionSpace(self.meshinfo.mesh, 'P', conditions.elementorder, (spatial_dims,spatial_dims))
-      self.expr = VaryingMatrixByCell(self.meshinfo.cells,conditions.mapping,spatial_dims,degree=0)
+      self.expr = expressions.VaryingMatrixByCell(self.meshinfo.cells,conditions.mapping,spatial_dims,degree=0)
     else:
       raise Exception("Invalid functiontype: %s"%ftype)
 
