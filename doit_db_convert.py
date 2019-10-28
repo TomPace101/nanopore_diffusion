6ebef69881ae87632499ac3bf177446257223cf0
Tom Pace <TomPace101@gmail.com>
2019-10-28 11:19:41 -0400

    First draft of the doit database alteration script

--- doit_db_convert.py
+++ doit_db_convert.py
@@ -3,13 +3,52 @@
 #Standard library
 from __future__ import print_function, division #Python 2 compatibility
 import argparse
+import dbm
+import json
 
 #Site packages
 
+#Constants
+ENCODING="utf-8"
+DIRECT_COPY_KEYS=['checker:','_values_:']
+
+def db_path_strip(infpath):
+  return infpath[:-3]
+
+def alter_task(indata,oldroot,newroot):
+  "Do the alteration for a single task"
+  outdata={}
+  #Each key is processed differently
+  for key,oldvalue in indata.items():
+    if key in DIRECT_COPY_KEYS:
+      #Direct copy
+      outdata[key]=oldvalue
+    elif key == 'deps:':
+      #Each entry in the list is an absolute path
+      newvalue=[fp.replace(oldroot,newroot) for fp in oldvalue]
+      outdata[key]=newvalue
+    else:
+      #The key itself is an absolute path
+      newkey=key.replace(oldroot,newroot)
+      outdata[newkey]=oldvalue
+  return outdata
 
 #Top-level function
 def do_alteration(inputfile,outputfile,oldroot,newroot):
-  pass
+  "Do the alteration for all the tasks in the database file"
+  #Open the input and output files
+  indb=dbm.open(db_path_strip(inputfile))
+  outdb=dbm.open(db_path_strip(outputfile),'c')
+  #Go through all the tasks
+  for task_id in indb.keys():
+    task_data=json.loads(indb[task_id].decode(ENCODING))
+    new_task_data=alter_task(task_data,oldroot,newroot)
+    outdb[task_id]=json.dumps(new_task_data).encode(ENCODING)
+  #Close databases
+  indb.close()
+  outdb.close()
+  #Done
+  return
 
 #Function to run from command line
 def command_line_run():
