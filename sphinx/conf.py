0e695277e9553e7f07a2c78b849271a7bb2d8b63
Tom Pace <TomPace101@gmail.com>
2020-06-12 17:20:21 -0400

    Allow autodoc to work without singularity

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -54,3 +54,12 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for autodoc
+
+autodoc_mock_imports = [
+  'fenics',
+  'ufl',
+  'matplotlib',
+  'pandas'
+]
