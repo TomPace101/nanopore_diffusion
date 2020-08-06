a2497ff35053fad9fce21c456c21b138cfe392d8
Tom Pace <TomPace101@gmail.com>
2020-08-06 13:34:05 -0400

    Make TemplateFileRequests a CommandSequence

--- simproc/requesthandler/templates.py
+++ simproc/requesthandler/templates.py
@@ -9,21 +9,24 @@ from jinja2 import Environment, FileSystemLoader
 
 #This package
 from . import yaml_manager
-from . import customization
+from . import commandseq
 from . import logging
 
 logger=logging.getLogger(__name__)
 
-_TemplateFileRequest_props_schema_yaml="""#GeneralShellCommandRequest
+_TemplateFileRequest_props_schema_yaml="""#TemplateFileRequest
 name: {type: string}
 searchpaths:
   type: array
   items: {type: pathlike}
 tmplfile: {type: pathlike}
 outfile: {type: pathlike}
-data: {type: object}"""
+data: {type: object}
+prepcommands: {type: array}
+postcommands: {type: array}
+"""
 
-class TemplateFileRequest(customization.CustomizableRequest):
+class TemplateFileRequest(commandseq.WithCommandsRequest):
   """General and base class for requests to fill in jinja2 template files
   
   You can subclass this by overriding the `get_template_input` method.
@@ -36,13 +39,33 @@ class TemplateFileRequest(customization.CustomizableRequest):
     - searchpaths: optional list of folders to add to the jinja2 search path for template inheritance
     - tmplfile: path to the input template file, as Path or string
     - outfile: path to the output file, as Path or string
-    - data: dictionary of data used to compute the template input values"""
+    - data: dictionary of data used to compute the template input values
+    - prepcommands = sequence of commands to execute before template generation (e.g. to load additional data)
+    - postcommands = sequence of commands to execute after template generation (e.g. to output additional data)"""
   _self_task=True
   _config_attrs=('tmplfile','outfile','data','modules','initializations','extra')
   _inputfile_attrs=['tmplfile']
   _outputfile_attrs=['outfile']
-  _validation_schema=customization.CustomizableRequest.update_schema(_TemplateFileRequest_props_schema_yaml)
+  _validation_schema=commandseq.WithCommandsRequest.update_schema(_TemplateFileRequest_props_schema_yaml)
   _validation_schema.required=['name','tmplfile','outfile','data']
+  def __init__(self,**kwargs):
+    #Initialization from base class
+    super(TemplateFileRequest, self).__init__(**kwargs)
+    #Default command attributes
+    if not hasattr(self,'prepcommands'):
+      self.prepcommands=[]
+    if not hasattr(self,'postcommands'):
+      self.postcommands=[]
+    #Get input files
+    self._more_inputfiles=getattr(self,'_more_inputfiles',[]) #Initialize attribute if it doesn't already exist
+    self._more_inputfiles+=[fp for k,fp in getattr(self,'loadfiles',{}).items()]
+    self._more_inputfiles+=self.list_iofiles(self.prepcommands,['filename','infpath'],'_inputfiles')
+    self._more_inputfiles+=self.list_iofiles(self.postcommands,['filename','infpath'],'_inputfiles')
+    #Get output files
+    self._more_outputfiles=getattr(self,'_more_outputfiles',[]) #Initialize attribute if it doesn't already exist
+    self._more_outputfiles+=[figprops.outfpath for figprops in getattr(self,'figures',[])]
+    self._more_outputfiles+=self.list_iofiles(self.prepcommands,['filename','outfpath'],'_outputfiles')
+    self._more_outputfiles+=self.list_iofiles(self.postcommands,['filename','outfpath'],'_outputfiles')
   def get_template_input(self):
     return self.data
   def run(self):
