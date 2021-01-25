c47d182407b6dd90eebc21e265d8f46f63de6370
Tom Pace <TomPace101@gmail.com>
2021-01-25 16:51:12 -0500

    Function to get a spatial integral from fenics

--- simproc/simulation/simrequest.py
+++ simproc/simulation/simrequest.py
@@ -406,6 +406,35 @@ class SimulationRequest(WithCommandsRequest):
     self.set_nested(attrpath,calcvol)
     return
 
+  def cell_integral(self,attrpath,pcell=None,funcpath=None):
+    """Compute the specified integral over the given cell.
+
+    Arguments:
+
+      - attrpath = attribute path for storage of result
+      - pcell = optional, physical cell number for the cell to calculate area of,
+          empty or None to use entire model
+      - funcpath = optional, path to function to integrate,
+          empty or None to use a constant value of 1.0
+
+    Required attributes:
+
+      - meshinfo.mesh = FEniCS Mesh object
+      - meshinfo.cells = FEniCS MeshFunction object for cell numbers
+    
+    No return value."""
+    if funcpath is None:
+      this_func=fem.Constant(1.0)
+    else:
+      this_func=self.get_nested(funcpath)
+    if pcell is None:
+      this_dx=fem.Measure('cell',domain=self.meshinfo.mesh)
+    else:
+      this_measure=fem.Measure('cell',domain=self.meshinfo.mesh,subdomain_data=self.meshinfo.cells)
+      this_dx=this_measure(pcell)
+    result=fem.assemble(this_func*this_dx)
+    self.set_nested(attrpath,result)
+
   def facet_area(self,pfacet,attrpath,internal=False):
     """Compute the area of the specified facet.
 
